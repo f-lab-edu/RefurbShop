6936adb4c004bb11e6eddd4a6410733e83a1c127
mingmong <mingmong@gmail.com>
2024-08-12 19:57:13 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/refurbshop/RefurbShopApplication.java
@@ -0,0 +1,13 @@
+package com.refurbshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefurbShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefurbShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/refurbshop/RefurbShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refurbshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefurbShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
